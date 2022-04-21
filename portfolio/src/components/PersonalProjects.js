daa7b32f47bf87d89cb1dcc36b5b25e5d7c58d38
mrthorpe <mrthorpe@umich.edu>
2022-04-21 16:36:47 -0400

    Add image to PersonalProjects. Add styling.

--- portfolio/src/components/PersonalProjects.js
+++ portfolio/src/components/PersonalProjects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import districtMap from "../images/school_district_map.PNG";
 
 const Personal = () => {
     return (
@@ -6,7 +7,7 @@ const Personal = () => {
             <h1>Personal Projects</h1>
             <h2>Cartography</h2>
             <p>My initial interest in maps is what led me to the field of information science.  Below is an example of my work using ESRI's ArcGIS suite.</p>
-            {/* insert images */}
+            <img className='img-responsive' id="map" src={districtMap} alt="Map of St. Louis area school districts"></img>
             <h2>Front-End Development</h2>
             <p>Below is an example of a travel website I built using HTML/CSS, with some JavaScript functionalities included.</p>
             <a href="https://mrthorpe12.github.io/si539-final-project/">Site Link</a>
