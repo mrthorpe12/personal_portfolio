395829c601362c7c10c8a03b9653e98ec488f994
mrthorpe <mrthorpe@umich.edu>
2022-10-15 11:31:31 -0400

    Add content to Working. Add images.

--- portfolio/src/components/PersonalProjects.js
+++ portfolio/src/components/PersonalProjects.js
@@ -1,5 +1,8 @@
 import React from "react";
 import districtMap from "../images/school_district_map.PNG";
+import si539final from "../images/si539_final_capture.JPG"
+import tops from "../images/tops_capture.JPG"
+import mtcf from "../images/mtcf_capture.JPG"
 
 const Personal = () => {
     return (
@@ -10,9 +13,21 @@ const Personal = () => {
             <img className='img-responsive' id="map" src={districtMap} alt="Map of St. Louis area school districts"></img>
             <h2>Front-End Development</h2>
             <p>Below is an example of a travel website I built using HTML/CSS, with some JavaScript functionalities included.</p>
+            <img className='img-responsive' id="map" src={si539final} alt="SI 539 final project"></img>
+            <br></br>
             <a href="https://mrthorpe12.github.io/si539-final-project/">Site Link</a>
-            <p>This semester, I built a website using ReactJS that uses an API to generate a list of rhymes or synonyms based on the user's input.</p>
-            <a href="https://mrthorpe12.github.io/si579-hw5-react/">Site Link</a>
+            <p>As part of my undergrad internship, I built and tested features for the University of Wisconsin Traffic Operations and Safety Laboratory's
+                Community Maps project.
+            </p>
+            <img className='img-responsive' id="map" src={tops} alt="TOPS website"></img>
+            <br></br>
+            <a href="https://transportal.cee.wisc.edu/partners/community-maps/crash/search/BasicSearch.do">Site Link</a>
+            <p>During my first semester at the University of Michigan, I built and tested features for the University of Michigan Transportation Research Institute's
+                Michigan Traffic Crash Facts website.
+            </p>
+            <img className='img-responsive' id="map" src={mtcf} alt="MTCF website"></img>
+            <br></br>
+            <a href="https://www.michigantrafficcrashfacts.org/querytool#q1;0;2021;;">Site Link</a>
         </div>
     )
 }
